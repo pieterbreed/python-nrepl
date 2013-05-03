cf4251bb6b2bf2fbdfd5c4500ae53d009aa5db2e
Pieter Breed <github.com@pb.co.za>
2013-05-03 19:42:47 +0200

    temp commit, busy unit testing the socket-thread function

--- tcp.py
+++ tcp.py
@@ -1,11 +1,18 @@
 # /usr/bin/env python
 
+import unittest, thread
+
 TCP_CHANNEL_TIMEOUT = 1 # seconds, float value
 TCP_READ_BUFFER_SIZE = 4098
 
-def socketThreadMain(host, port, sendQueue, receiveQueue, stoppedEvent):
-	'''this method will perform the communications with (host, port)
+def socketThreadMain(socket, sendQueue, receiveQueue, stoppedEvent):
+	'''this method will perform the communications with a socket-like object
 	and perform sending and receiving of data via the passed Queues.
+
+	socket => an object supporting send(byte[]) : int, recv(int) : byte[] & close()
+
+	sendQueue => a Queue object with which this thread is controlled
+
 	SendQueue expects dicts for instructions:
 
 	two types are supported: 
@@ -21,15 +28,15 @@ def socketThreadMain(host, port, sendQueue, receiveQueue, stoppedEvent):
 		"contents": string/bytes to be sent
 	}
 
-	receiveQueue contains raw bytes/string read from the socket
+	receiveQueue => a queue containing raw bytes/string read from the socket
+
+	stoppedEvent => an Event that signals when the thread has terminated
 
 	'''
 
-	s = socket.create_connection((host, port), TCP_CHANNEL_TIMEOUT)
-
 	mustStop = False
 	received = ''
-	while !mustStop:
+	while not mustStop:
 
 		# sends everything that is in the send queue first
 		hasStuffToSend = True
@@ -44,7 +51,7 @@ def socketThreadMain(host, port, sendQueue, receiveQueue, stoppedEvent):
 				elif stufftoSend['type'] == 'message':
 					messageContents = stufftoSend['contens']
 					while len(messageContents) > 0:
-						sent = s.send(messageContents)
+						sent = socket.send(messageContents)
 						messageContents = messageContents[sent:]
 			except Queue.Empty, e:
 				hasStuffToSend = False
@@ -52,13 +59,13 @@ def socketThreadMain(host, port, sendQueue, receiveQueue, stoppedEvent):
 		# if we don't have anything else to send
 		# and we did not get a request to stop then 
 		# lets try reading for a while
-		if !mustStop:
+		if not mustStop:
 			moreToRead = True
 			# try to read everything from the socket
 			# that we can read now without waiting to long for
 			while moreToRead:
 				try:
-					received = received + s.recv(TCP_READ_BUFFER_SIZE)
+					received = received + socket.recv(TCP_READ_BUFFER_SIZE)
 				except socket.timeout:
 					moreToRead = False
 
@@ -74,8 +81,7 @@ def socketThreadMain(host, port, sendQueue, receiveQueue, stoppedEvent):
 				pass
 
 
-	s.shutdown()
-	s.close()
+	socket.close()
 	stoppedEvent.set()
 
 class Tcp:
@@ -118,11 +124,86 @@ class Tcp:
 		everything from the sockets received queue and returns it to the caller'''
 		self._processReceivesLock.acquire()
 		received = ''
-		while !self._socketReceiveQueue.empty():
+		while not self._socketReceiveQueue.empty():
 			try:
 				received = received + self._socketReceiveQueue.qet_nowait()
 			except Queue.Empty:
 				pass
 		self._processReceivesLock.release()
 		return received
+
+class MockSocket:
+
+	def __init__(self, recvs):
+		'''recvs => list of items. items must be either strings or the value True.
+		When True is read, a Queue.Empty will be raised
+
+		sends => similarly a list of items. items must be strings or the Value True.
+		When True is encountered, a Queue.Empty is raised'''
+		self._recvs = recvs
+		self._sends = []
+		self._closeCalled = False
+
+
+	def send(self, bs):
+		self._sends.append(bs)
+
+	def recv(self, i):
+		res = self._recvs.pop()
+		return res
+
+	def close(self):
+		self._closeCalled = True
+
+class MockQueue:
+
+	def __init__(self, gets, putsAllows):
+		'''gets => a list of things that will be read. Must be either a None or 
+		python item. For every None a Queue.Empty will be raised
+
+		putsAllows => an item corresponding to every puts invocation. True
+		accepts the put and False raises an Queue.Full'''
+		self._gets = gets
+		self._puts = []
+		self._putsAllows = putsAllows
+
+	def get_nowait(self):
+		return self._gets.pop()
+
+	def put_nowait(self, received):
+		allow = self._putsAllows.pop()
+		if allow:
+			self._puts.append(received)
+		else
+			raise Queue.Full
+
+
+class TcpTests(unittest.TestCase):
+	def test_thread_read_1():
+		socketReceives = ['aoeu']
+		socket = MockSocket(socketReceives)
+
+		sendQueue = MockQueue([False, {'type': 'control', 'op': 'stop'}], None)
+		receiveQueue = MockQueue(None, [True])
+		
+		stoppedEvent = thread.Event()
+
+		socketThreadMain(socket, sendQueue, receiveQueue, stoppedEvent)
+
+		self.assertTrue(stoppedEvent.is_set())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
